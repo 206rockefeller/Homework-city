0abe9f4c7d71f109ae2f09c2a7d1864c6e8627db
206rockefeller <rocky.little@sublimemedia.com>
2015-11-01 15:20:45 -0800

    refined code

--- index.js
+++ index.js
@@ -2,49 +2,57 @@ $(document).ready(function() {
 	console.log("ready!");
 
 
-			// when input in search bar trigger event listener 
+	// when input in search bar trigger event listener 
 
-			$("form").submit(function(){
-				event.preventDefault();
-				
-				// capture the city the user inputs 
-				
-				var userCity = $("#city-type").val(); 
+	$("form").submit(function(e){
+		e.preventDefault();
+		
+		// capture the city the user inputs 
+		
+		var userCity = $("#city-type").val().toLowerCase();
 
-				// If a user enters "New York" or "New York City" or "NYC" make the background of the page nyc.jpg
-				
-				if (userCity == "New York" || userCity == "New York City" || userCity == "NYC") {
-					$("body").css("background-image", "url(images/nyc.jpg");
-				}
-				
-				// If a user enters "San Francisco" or "SF" or "Bay Area" make the background of the page sf.jpg
-				
-				else if (userCity == "San Francisco" || userCity == "SF" || userCity == "Bay Area") {
-					$("body").css("background-image", "url(images/sf.jpg");
-				}
-				
-				// If a user enters "Los Angeles" or "LA" or "LAX" make the background of the page la.jpg
-				
-				else if (userCity == "Los Angeles" || userCity == "LA" || userCity == "LAX") {
-					$("body").css("background-image", "url(images/la.jpg");
-				}
+		var brickWall;
 
-				// If a user enters "Austin" or "ATX" make the background of the page austin.jpg
-				
-				else if (userCity == "Austin" || userCity == "ATX") {
-					$("body").css("background-image", "url(images/austin.jpg");
-				}
+		// If a user enters "New York" or "New York City" or "NYC" make the background of the page nyc.jpg
+		
+		if (userCity === "new york" || userCity === "new york city" || userCity === "nyc") {
+			brickWall = "nyc";
+		}
 
-				// If a user enters "Sydney" or "SYD" make the background of the page sydney.jpg
+		
+		// If a user enters "San Francisco" or "SF" or "Bay Area" make the background of the page sf.jpg
+		
+		else if (userCity === "san francisco" || userCity === "sf" || userCity === "bay area") {
+			brickWall = "sf";
+		}
+		
+		// If a user enters "Los Angeles" or "LA" or "LAX" make the background of the page la.jpg
+		
+		else if (userCity === "los angeles" || userCity === "la" || userCity === "lax") {
+			brickWall = "la";
+		}
 
-				else if (userCity == "Sydney" || userCity == "SYD") {
-					$("body").css("background-image", "url(images/sydney.jpg");
-				}
+		// If a user enters "Austin" or "ATX" make the background of the page austin.jpg
+		
+		else if (userCity === "austin" || userCity === "atx") {
+			brickWall = "austin";
+		}
 
-				// If user enters an invalid city display error msg 
-			}) 
+		// If a user enters "Sydney" or "SYD" make the background of the page sydney.jpg
 
-			
+		else if (userCity === "sydney" || userCity === "syd") {
+			brickWall = "sydney";
+		}
+		// If user enters an invalid city display error msg 
 
+		if (brickWall) {
+			$("body").css("background-image", "url(images/" + brickWall +".jpg)");
+		} else {
+			alert('HEY STUPID!');
+		}
 
+		
+		$(this).trigger("reset");
+
+	});
 });
